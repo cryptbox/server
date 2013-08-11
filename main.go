a0105a96f7bc052c575c1e3e8d341125884ddc74
Shane Iler <sriler@gmail.com>
2013-08-11 02:05:05 -0400

    add file download, make more messy

--- main.go
+++ main.go
@@ -1,41 +1,107 @@
 package main
 
 import (
-	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/ant0ine/go-json-rest"
+	"log"
 	"net/http"
 	"os"
+	"io"
 	"path/filepath"
 )
 
 var rootPath = flag.String("Root Folder", ".", "Root folder to share")
 
-type Object struct {
-	IsDir bool `json:"isDir"`
-	Name  string `json:"name"`
-	Size  int64 `json:"size"`
+type FileList struct {
+	Path string `json:"path"`
+	More bool   `json:"more"`
+	Children []*File `json:"children"`
 }
 
-func GetDirs(w *rest.ResponseWriter, req *rest.Request) {
-	relPath, _ := base64.StdEncoding.DecodeString(req.PathParam("path"))
-	path := filepath.Join(*rootPath, string(relPath))
+func NewFileList(path string) *FileList {
+	response := new(FileList)
+	response.Children = make([]*File, 0)
 
-	fmt.Println("Received request for: ", path)
+	return response
+}
+
+func (l *FileList) LoadFrom(dir *os.File) {
+	subDirs, err := dir.Readdir(100)
+
+	if err == io.EOF {
+		l.More = false
+	}
+
+	for _, subDir := range subDirs {
+		l.AddFile(&File{subDir.IsDir(), subDir.Name(), subDir.Size()})
+	}
+}
+
+func (r *FileList) AddFile(o *File) {
+	r.Children = append(r.Children, o)
+}
+
+type File struct {
+	IsDir bool   `json:"isDir"`
+	Name  string `json:"name"`
+	Size  int64  `json:"size"`
+}
+
+func DirsHandler(w http.ResponseWriter, req *http.Request) {
+	path := filepath.Join(*rootPath, req.FormValue("path"))
+
+	fmt.Println("Received dir request for: ", path)
 
 	dir, err := os.Open(path)
 	if err != nil {
 		return
 	}
 
-	subDirs, err := dir.Readdir(100)
-	results := make([]*Object, 0, len(subDirs))
-	for _, subDir := range subDirs {
-		results = append(results, &Object{subDir.IsDir(), subDir.Name(), subDir.Size()})
+	dirStat, err := dir.Stat()
+	if err != nil {
+		return
 	}
 
-	w.WriteJson(&results)
+	response := NewFileList(path)
+	if dirStat.IsDir() {
+		response.LoadFrom(dir)
+	}
+
+	bytes, _ := json.Marshal(response)
+	w.Write(bytes)
+}
+
+func FileHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Disposition", fmt.Sprint("attachment; filename=\"", req.FormValue("path"), "\""))
+	w.Header().Set("Content-Transfer-Encoding", "binary")
+
+	fmt.Printf("%#v\n", w.Header())
+	fmt.Println(*rootPath)
+
+	path := filepath.Join(*rootPath, req.FormValue("path"))
+
+	fmt.Println("Received file request for: ", path)
+
+	dir, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Unable to open file: ", path)
+		return
+	}
+
+	dirStat, err := dir.Stat()
+	if err != nil {
+		fmt.Println("Unable to stat file: ", path)
+		return
+	}
+
+	if dirStat.IsDir() {
+		fmt.Println("Object is file: ", path)
+		return
+	}
+
+	io.Copy(w, dir)
 }
 
 func main() {
@@ -47,11 +113,10 @@ func main() {
 		}
 	}
 	path, _ := filepath.Abs(*rootPath)
+	rootPath = &path
 	fmt.Println("Serving: ", path)
 
-	handler := rest.ResourceHandler{}
-	handler.SetRoutes(
-		rest.Route{"GET", "/dirs/:path", GetDirs},
-	)
-	http.ListenAndServe(":5555", &handler)
+	http.HandleFunc("/dirs/", DirsHandler)
+	http.HandleFunc("/file/", FileHandler)
+	log.Fatal(http.ListenAndServe(":5555", nil))
 }
